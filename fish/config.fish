deb45a50fdbb547f9e4ff5258070f7f334b349b5
vgxl <vgxl@git.hub>
2024-11-18 18:43:00 +0700

    change config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,12 @@ fish_add_path -m ~/.local/bin
 
 set -x XMODIFIERS @im=fcitx
 set -x QT_IM_MODULE fcitx
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
